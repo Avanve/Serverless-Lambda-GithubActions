e4f7fedf356e2060fbe0bd282d7dd37b8b3114ee
Avanve <Avanve>
2023-04-24 12:02:54 -0700

    commit

--- src/handler.js
+++ src/handler.js
@@ -1,10 +1,7 @@
 'use strict';
 
 module.exports.generateRandomNumber = event => {
- const randomNumber = parseInt(Math.random() * 100);
- console.log('random generated integer is: ',randomNumber);
- return randomNumber;
+  const randomNumber = parseInt(Math.random() * 100);
+  console.log("The random generated integer is: ", randomNumber);
+  return randomNumber;
 };
-
-
-
